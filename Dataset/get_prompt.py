a12b49b4444a78e27c10b9993890209c0e24464d
陈实 <chenshi020522@outlook.com>
2023-06-06 00:59:22 +0800

    23/06/06: training data, reivew report

--- Dataset/get_prompt.py
+++ Dataset/get_prompt.py
@@ -11,9 +11,11 @@ concat3 = ".png\", \"prompt\": \""
 concat4 = "\"}"
 
 # output file 
-output_file_path = "./prompt.json"
+output_file_path = "./training/danbooruSmall/prompt.json"
 # open file path
-open_file_path = "./prompts/"
+open_file_path = "./training/danbooruSmall/prompts/"
+# pictures file path
+pic_file_path = "./training/danbooruSmall/source/"
 
 """
 遍历 prompts 文件夹内文件
@@ -23,21 +25,52 @@ open_file_path = "./prompts/"
 def get_prompt():
     # open the output temp file
     output_file = open(output_file_path, 'w', encoding = 'utf-8')
-    # 循环遍历
-    for file in os.listdir(open_file_path):
-        (filename,file_extension_name) = os.path.splitext(file)
-        # open the current file
-        current_file = open(open_file_path + file)
+
+    # 获取文件夹中存在的 pic 的文件名（共 10000 个）
+    pic_filenames = []
+    for pic_file in os.listdir(pic_file_path):
+        (pic_filename, file_extension_name) = os.path.splitext(pic_file)
+        pic_filenames.append(pic_filename)
+    # print(pic_filenames)
+    # print(len(pic_filenames))
+
+    for pic_filename in pic_filenames:
+        # open prompt file
+        prompt_file = open(open_file_path + pic_filename + ".txt")
         # get the one line of the file
-        line = current_file.readline()
-        # 去除逗号，去除反斜杠
+        line = prompt_file.readline()
+        # 去除逗号，去除反斜杠，去除 ""
         # line = line.replace(",", "")
         line = line.replace("\\", "")
+        line = line.replace("\"", "\'")
         # print(open_file_path + file + line)
         # write into the output file in format
-        output_file.write(concat1 + filename + concat2 + filename + concat3 + line + concat4 + "\n")
+        output_file.write(concat1 + pic_filename + concat2 + pic_filename + concat3 + line + concat4 + "\n")
         # close 
-        current_file.close()
+        prompt_file.close()
+
+    
+    # 循环遍历
+    # for file in os.listdir(open_file_path):
+    #     (filename,file_extension_name) = os.path.splitext(file)
+        
+    #     # 判断当前的 filename 是否在 pic_filenames 中已存在
+    #     if (filename not in pic_filenames):
+    #         continue
+
+    #     # open the current file
+    #     current_file = open(open_file_path + file)
+    #     # get the one line of the file
+    #     line = current_file.readline()
+    #     # 去除逗号，去除反斜杠，去除 ""
+    #     # line = line.replace(",", "")
+    #     line = line.replace("\\", "")
+    #     line = line.replace("\"", "\'")
+    #     # print(open_file_path + file + line)
+    #     # write into the output file in format
+    #     output_file.write(concat1 + filename + concat2 + filename + concat3 + line + concat4 + "\n")
+    #     # close 
+    #     current_file.close()
 
     # close 
     output_file.close()
